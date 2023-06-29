87ace11feb5cbd25dbfa7d3032eece4c18915de5
Gopal <gopalroy5397@gmail.com>
2023-06-29 21:44:13 +0530

    made site responsive

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const phone = document.getElementById("number")
 const email = document.getElementById("email")
 let connectContainer = document.getElementById("connect-container");
 const para = document.getElementById("form-submit-para");
+// const menuItems = document.querySelector("menu-mobile-items");
+const menuItems = document.getElementById("menu-mobile-items");
+
 submitBtn.addEventListener("click",submitFun);
+var counter=0;
+let clicked =1;
+menuItems.style.display="none";
+
+
+const slides = document.querySelectorAll(".slides");
 
 function submitFun(){
     if (name.value.length<=0) {
@@ -25,3 +34,59 @@ function submitFun(){
 
 
 }
+
+//logic for sliding of images
+
+//for making images come one  after another in x direction
+// and then making their overflow hidden using parent 
+slides.forEach(
+    (slide,index)=>{
+        slide.style.left = `${index  * 100}%`;
+    }
+)
+
+const goNext=()=>{
+    if(counter==slides.length-1){
+        counter=0;
+    }
+    else{
+
+        counter++;
+    }
+    slideImage();
+    console.log("nexr");
+}
+const goPrev=()=>{
+    if(counter==0){
+        counter=slides.length-1;
+    }
+    else{
+
+        counter--;
+    }
+    slideImage();
+}
+
+const slideImage=()=>{
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+    
+}
+
+//logic for menu clicked 
+const menuClicked=()=>{
+    if (clicked===0) {
+        clicked=1;
+        menuItems.style.display="none";
+    }
+    else{
+        clicked=0;
+        menuItems.style.display="block";
+    }
+}
+setInterval(goNext,2000 );
+
+
